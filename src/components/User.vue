0b9778a0881d1ed69802928b30efd968168327c0
kenini1805 <huusu1996@gmail.com>
2018-08-07 08:45:23 +0700

    init vue cli

---
+++ src/components/User.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="component">
+        <p>Ten toi la {{ name }}</p>
+        <p>Toi nam nay {{ age }} tuoi</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-6">
+            <button @click="resetAge">Reset tuoi</button>
+            <app-user-detail :age="age" @ageWasUpdated="age = $event"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-6">
+                <app-user-edit :user-age="age" @ageWasEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function () {
+            return {
+                name: 'Su',
+                age: 23
+            };
+        },
+
+        methods: {
+            resetAge () {
+                this.age = 23
+            }
+        },
+
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
